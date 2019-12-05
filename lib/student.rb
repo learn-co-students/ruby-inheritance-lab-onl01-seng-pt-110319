e43dcf607390f4c8e15cafb7efd1dc0317a334e6
Wills Blake <richwblake@gmail.com>
2019-12-04 22:14:39 -0500

    Final commit, all tests pass

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < Teacher
 
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(information)
+    @knowledge << information
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
