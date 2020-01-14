c55076e9c5e8d04b009a38d109cb813456a39dc5
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2020-01-14 06:41:00 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+
+  
+  def initialize()
+    @knowledge = []
+  end
+  
+  def learn(stringOfKnowledge)
+    @knowledge << stringOfKnowledge
+  end
+  
+  def knowledge
+    @knowledge
+  end
 
 end
